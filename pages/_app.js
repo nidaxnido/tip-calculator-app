2b949c4536baf708f060a4bc886dc1d16504718e
nidaxnido <nidabelajar@gmail.com>
2023-02-22 03:59:19 +0700

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import '@/styles/globals.css'
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+
+const theme =extendTheme({
+  colors:{
+    brand:{
+      cyan : "hsl(172, 67%, 45%)",
+      darkCyan: "hsl(183, 100%, 15%)",
+      greyCyan: "hsl(184, 14%, 56%)",
+      darkGreyCyan : "hsl(186, 14%, 43%)",
+      liteGreyCyan: "hsl(185, 41%, 84%)",
+      paleGreyCyan : "hsl(189, 41%, 97%)",
+      white: "White: hsl(0, 0%, 100%)",
+    }
+  }
+})
+export default function App({ Component, pageProps }) {
+  return <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+  </ChakraProvider>
+  
+}
